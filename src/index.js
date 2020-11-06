a7911ff2d4dc49d194350473068d4ea598c9387f
leejiheum <gihum@incar.co.kr>
2020-11-06 17:55:19 +0900

    default 경로 제거

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';//GitHub pages를 사용할 때는 HashRouter를 사용한다.
+// import { BrowserRouter } from 'react-router-dom';
 import Route from './route';
 
 ReactDOM.render(
-	<BrowserRouter>
+	// <BrowserRouter>
+	<HashRouter>
 		<Route />
-	</BrowserRouter>,
+	</HashRouter>,
+	// </BrowserRouter>,
 	document.getElementById('root'),
 );
